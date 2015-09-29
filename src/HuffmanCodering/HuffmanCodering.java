965bab8e0a299a970b962adf26133cea86d100b6
Thomas Kleinendorst <Thomas Kleinendorst@DESKTOP-K8K66EJ>
2015-09-29 17:42:24 +0200

    Nou, daar is verdomme weer een hoop code bijgekomen! Wat een geclown!!!

--- src/HuffmanCodering/HuffmanCodering.java
+++ src/HuffmanCodering/HuffmanCodering.java
@@ -1,25 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package HuffmanCodering;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
 
-/**
- *
- * @author Niek
- */
 public class HuffmanCodering {
-
-    private Map<Character, String> codesMap;
-
     public HuffmanCodering(String word) {
+        /// Step 1 counting the frequency of characters ///
+        
         //Print the word
         System.out.println("Word to compress: " + word + "\n");
           //Put unique words together with their frequency in a list
@@ -28,10 +15,72 @@ public class HuffmanCodering {
         {
             System.out.println(cf); 
         }
+        
+        /// END OF STEP 1 ///
+        
+        /// Step 2 Sorting the characters on frequency
+        
+        freqList.sort(null);
+        System.out.println("");
+        for(CharFrequency f : freqList) {
+            System.out.println(f.toString());
+        }
+        
+        /// END OF STEP 2 ///
+        
+        /// Step 3 creating the Huffman tree
+        // making a list of huffmannode objects
+        List<HuffmanNode> nodes = new ArrayList<>();
+        
+        // fill the nodes list with previous char objects
+        for(CharFrequency chr : freqList) {
+            nodes.add(new HuffmanNode(chr));
+        }
+        
+        // keep looping whilst the nodes list contains more than 1 node.
+        // if it only has one node than this is the top of the tree,
+        // which contains all information about children notes, and
+        // therefore contains information about the while tree
+        while(nodes.size() != 1) {
+            // sorts the smallest frequency nodes on the bottom of the list
+            nodes.sort(null);
+            
+            // gets the lowest level nodes
+            HuffmanNode leftChild = nodes.get(0);
+            HuffmanNode rightChild = nodes.get(1);
+            
+            // removes old nodes from list
+            nodes.remove(leftChild);
+            nodes.remove(rightChild);
+            
+            // adds new node to list
+            nodes.add(new HuffmanNode(leftChild, rightChild));
+            
+            for(HuffmanNode node : nodes) {
+                System.out.println(node.toString());
+            }
+            
+            System.out.println("");
+        }
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+        /// END OF STEP 3 ///
     }
-
-
-
+    
     /**
      * Converts a List of characters to a List of CharFrequency objects which
      * contain a character and the frequency of it's appearance.
@@ -62,5 +111,4 @@ public class HuffmanCodering {
     public static void main(String[] args) {
         HuffmanCodering huffcode = new HuffmanCodering("kunstgeschiedenis");
     }
-
 }
---
+++ src/HuffmanCodering/HuffmanNode.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HuffmanCodering;
+
+import java.util.List;
+
+/**
+ *
+ * @author Thomas Kleinendorst
+ */
+public class HuffmanNode implements Comparable<HuffmanNode> {
+    private int frequency;
+    private boolean isCharacterNode;
+    private CharFrequency chr;
+    
+    private HuffmanNode leftChildNode, rightChildNode;
+    
+    
+    public HuffmanNode(HuffmanNode leftChildNode, HuffmanNode rightChildNode) {
+        this.leftChildNode = leftChildNode;
+        this.rightChildNode = rightChildNode;
+  
+        frequency = 0;
+        frequency += leftChildNode.getFrequency() + rightChildNode.getFrequency();
+        
+        isCharacterNode = false;
+    }
+    
+    public HuffmanNode(CharFrequency charFrequency) {
+        this.chr = charFrequency;
+        
+        frequency = charFrequency.getFrequency();
+        
+        isCharacterNode = true;
+    }
+    
+    public int getFrequency() {
+        return this.frequency;
+    }
+    
+    public CharFrequency getCharacter() {
+        if(isCharacterNode) {
+            return chr;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public int compareTo(HuffmanNode o) {
+        if(this.getFrequency() > o.getFrequency()) {
+            return 1;
+        } else if(this.getFrequency() == o.getFrequency()) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+    
+    @Override
+    public String toString() {
+        return Integer.toString(frequency);
+    }
+}
