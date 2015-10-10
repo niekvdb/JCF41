01fc963da01630fd3d5154eef81e3b43fedc0a73
Thomas Kleinendorst <Thomas Kleinendorst@DESKTOP-K8K66EJ>
2015-10-10 14:29:31 +0200

    Het ontcijferen van het bericht werkt nu met de nieuwe methode. Dit wil zeggen dat hij het boom object meekrijgt en aan de hand hiervan de code ontcijferd, dit heeft veel tijdswinst tot gevolg!

--- src/HuffmanCodering/HuffmanCodering.java
+++ src/HuffmanCodering/HuffmanCodering.java
@@ -22,7 +22,7 @@ public class HuffmanCodering {
 
         /// END OF STEP 1 ///
         /// Step 2 Sorting the characters on frequency /// 
-        freqList.sort(null);
+        freqList = sortCharFrequencyList(freqList);
         System.out.println("");
         for (CharFrequency f : freqList) {
             System.out.println(f.toString());
@@ -30,46 +30,14 @@ public class HuffmanCodering {
 
         /// END OF STEP 2 ///
         /// Step 3 creating the Huffman tree
-        // making a list of huffmannode objects
-        List<HuffmanNode> nodes = new ArrayList<>();
-
-        // fill the nodes list with previous char objects
-        for (CharFrequency chr : freqList) {
-            nodes.add(new HuffmanNode(chr));
-        }
-
-        // keep looping whilst the nodes list contains more than 1 node.
-        // if it only has one node than this is the top of the tree,
-        // which contains all information about children notes, and
-        // therefore contains information about the while tree
-        while (nodes.size() != 1) {
-            // sorts the smallest frequency nodes on the bottom of the list
-            nodes.sort(null);
-
-            // gets the lowest level nodes
-            HuffmanNode leftChild = nodes.get(0);
-            HuffmanNode rightChild = nodes.get(1);
-
-            // removes old nodes from list
-            nodes.remove(leftChild);
-            nodes.remove(rightChild);
-
-            // adds new node to list
-            nodes.add(new HuffmanNode(leftChild, rightChild));
-
-            for (HuffmanNode node : nodes) {
-                System.out.println(node.toString());
-            }
-
-            System.out.println("");
-        }
+        HuffmanNode tree = createTree(freqList);
 
         /// END OF STEP 3 ///
         /// Step 4 reading the codes from the tree ///
         //Initialize code map
         codesMap = new HashMap<>();
         //Put the codes for each letter in 'codesMap'
-        getCodes(nodes.get(0), "");
+        getCodes(tree, "");
         //Print all codes
         System.out.println("Character codes: ");
 
@@ -87,37 +55,56 @@ public class HuffmanCodering {
         
         /// END OF STEP 5 ///
         
-        /// step 6 Decode message and print it ///
-        // verwijzing naar functie
-        
-        Map<String, Character> decodeMap = new HashMap<>();
-        
-        for(Map.Entry<Character, String> entry : codesMap.entrySet()){
-            decodeMap.put(entry.getValue(), entry.getKey());
-        }
-        
-        System.out.println(decodeMessage(codedMessage, decodeMap));
+        /// step 6 Decode message and print it ///        
+        System.out.println(decodeMessage(codedMessage, tree));
         
         /// END OF STEP 6 ///
     }
     
-    private String decodeMessage(String message, Map<String, Character> decodeMap) {
-        String todoMessage = message;
-        String finalMessage = "";
-        while(todoMessage.length() > 0) {
-            boolean characterDone = false;
-            int charactersToRead = 1;
-            while(!characterDone) {
-                if(decodeMap.containsKey(todoMessage.substring(0, charactersToRead))) {
-                    characterDone = true;
-                    finalMessage += decodeMap.get(todoMessage.substring(0, charactersToRead));
+    private List<CharFrequency> sortCharFrequencyList(List<CharFrequency> freqList) {
+        freqList.sort(null);
+        return freqList;
+    }
+    
+    private String decodeMessage(String inputMessage, HuffmanNode tree) {
+        // Create a string which will hold the message that needs to be decoded further
+        String message = inputMessage;
+        // A string which will contain the decoded message
+        String result = "";
+        
+        // keep looping until all of the message is decoded
+        while(message.length() > 0) {
+            // an integer to keep track of the working character in
+            // the message which needs to be decoded
+            int characterCount = 0;
+            // place where the decoded character is placed in
+            CharFrequency character = null;
+            // The working node of that loop step
+            HuffmanNode node = tree;
+            
+            while(character == null) {
+                if(!node.getIsCharacterNode()) {
+                    // check if the next node is the left or right branch in the tree
+                    if("0".equals(message.substring(characterCount, characterCount + 1))) {
+                        node = node.getLeftChildNode();
+                    } else if("1".equals(message.substring(characterCount, characterCount + 1))) {
+                        node = node.getRightChildNode();
+                    } else {
+                        throw new IllegalArgumentException("The input string does not consist of only binairy information!");
+                    }
+                    
+                    characterCount++;
                 } else {
-                    charactersToRead++;
+                    // character is found!!
+                    character = node.getCharacter();
                 }
             }
-            todoMessage = todoMessage.substring(charactersToRead, todoMessage.length());
+            
+            result += character.getCharacter();
+            message = message.substring(characterCount);
         }
-        return finalMessage;
+        
+        return result;
     }
     
     /**
@@ -185,5 +172,43 @@ public class HuffmanCodering {
      */
     public static void main(String[] args) {
         HuffmanCodering huffcode = new HuffmanCodering("kunstgeschiedenis");
+    }
+
+    private HuffmanNode createTree(List<CharFrequency> freqList) {
+        // making a list of huffmannode objects
+        List<HuffmanNode> nodes = new ArrayList<>();
+
+        // fill the nodes list with previous char objects
+        for (CharFrequency chr : freqList) {
+            nodes.add(new HuffmanNode(chr));
+        }
+
+        // keep looping whilst the nodes list contains more than 1 node.
+        // if it only has one node than this is the top of the tree,
+        // which contains all information about children notes, and
+        // therefore contains information about the while tree
+        while (nodes.size() != 1) {
+            // sorts the smallest frequency nodes on the bottom of the list
+            nodes.sort(null);
+
+            // gets the lowest level nodes
+            HuffmanNode leftChild = nodes.get(0);
+            HuffmanNode rightChild = nodes.get(1);
+
+            // removes old nodes from list
+            nodes.remove(leftChild);
+            nodes.remove(rightChild);
+
+            // adds new node to list
+            nodes.add(new HuffmanNode(leftChild, rightChild));
+
+            for (HuffmanNode node : nodes) {
+                System.out.println(node.toString());
+            }
+
+            System.out.println("");
+        }
+        
+        return nodes.get(0);
     }
 }
